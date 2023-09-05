3db59b30d02e4169167ba88a74865fd658668926
Lokendra Kushwah <118094744+Lokendrakushwah12@users.noreply.github.com>
2023-09-05 11:32:08 +0530

    fixed-the-ui

---
+++ script.js
@@ -0,0 +1,7 @@
+// const btn=document.querySelector('.input');
+
+// btn.addEventListener('click',function(){
+//     btn.style.border="2px solid #ffff";
+//     btn.style.borderRadius="10px";
+//     console.log(10);
+// })
