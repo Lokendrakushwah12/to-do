2b4b39a7361273ce475f7bcfad322608e55630d7
Lokendra Kushwah <118094744+Lokendrakushwah12@users.noreply.github.com>
2023-09-05 13:16:39 +0530

    added-like-btn

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-// const btn=document.querySelector('.input');
+const input_field = document.querySelector('.input');
 
-// btn.addEventListener('click',function(){
-//     btn.style.border="2px solid #ffff";
-//     btn.style.borderRadius="10px";
-//     console.log(10);
-// })
+document.addEventListener('click', function (e) {
+    if (input_field.contains(e.target)) {
+        // click inside
+        input_field.style.border = "2px solid rgba(220, 20, 60, 0.5)";
+    } else {
+        // click outside
+        input_field.style.border = "2px solid rgba(220, 20, 60, 0.2)";
+    }
+});
+
+const rating = document.querySelector('.rating');
+const logo = document.querySelector('.logo');
+
+rating.addEventListener('click', function () {
+    logo.style.opacity = "0.9";
+    logo.style.transform = " translate(-50%, -50%) scale(1)";
+    setTimeout(() => {
+        logo.style.opacity = "0";
+        logo.style.transform = " translate(-50%, -50%) scale(0.2)";
+    }, 1000);
+});
